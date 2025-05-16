a0b485f19a873f21d14c60d249f7467e06b6d550
kurokaw3a <kamaldinov321@gmail.com>
2025-05-16 16:28:01 +0600

    master

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ def get_connection():
     connection = sqlite3.connect("ocean.db")
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS Bot (id INTEGER PRIMARY KEY, admin TEXT NOT NULL, props TEXT NOT NULL)''')
-    cursor.execute('INSERT OR IGNORE INTO Bot (id, admin, props) VALUES (?, ?, ?)', (1, 'ocean_sup', "996100200300"))
+    cursor.execute('INSERT OR IGNORE INTO Bot (id, admin, props) VALUES (?, ?, ?)', (1, 'ccck7779', "996100200300"))
     cursor.execute('''CREATE TABLE IF NOT EXISTS Props (id INTEGER PRIMARY KEY, props TEXT)''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, username TEXT, xid INTEGER)''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS Payments (date TEXT, id INTEGER, username TEXT, xid INTEGER, sum INTEGER, method TEXT)''')
