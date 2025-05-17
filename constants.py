00493c7ee713ed364b37c380239029d18500c908
kurokaw3a <kamaldinov321@gmail.com>
2025-05-17 17:28:47 +0600

    master

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ data = database.get_bot_data()
 
 bot_admin = data["admin"] or "none"
 bot_props = data["props"] or "none"
-bot_new_props = "none"
+bot_new_props = ""
 
 channel = "-1002563085447"
 channel_link = "https://t.me/ocean_kg"
